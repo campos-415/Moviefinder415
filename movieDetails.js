8712a3a348d3486471d8117b11398a2586b4eaeb
Cesar Campos <camposss.415@gmail.com>
2023-01-21 21:25:11 -0800

    .

---
+++ movieDetails.js
@@ -0,0 +1,25 @@
+let movieInfo = localStorage.getItem('movieTitle')
+
+async function showMovieDetails(movieInfo) {
+  const movieDetailWrapper = document.querySelector('.movie')
+  const movieResponse = await fetch(`https://www.omdbapi.com/?apikey=84e956e5&i=${movieInfo}`)
+  const movieData = await movieResponse.json()
+  const movieDetails = movieData
+  let movieDetailsHTML = showDetails(movieDetails)
+  // movieDetailWrapper.innerHTML += movieDetailsHTML
+  console.log(movieDetailsHTML)
+}
+
+
+function showDetails(movie){
+  return `<div class="container">
+  <div class="row">
+    <h2 class="movie__title">${movie.Title}</h2>
+    <figure class="movie__img--wrapper">
+      <img src="${movie.Poster}" alt="" class="movie__img">
+    </figure>
+    <p class="movie__plot">${movie.Plot}</p>
+  </div>
+</div>`
+}
+showMovieDetails(movieInfo)
--- movies.js
+++ movies.js
@@ -1,26 +1,31 @@
-// const userSearch = localStorage.getItem("movieName");
-async function fetchMovie() {
+const userSearch = localStorage.getItem("movieName");
+async function fetchMovie(userSearch) {
   const movieResponse = await fetch(
-    `https://www.omdbapi.com/?apikey=84e956e5&s=fast`
+    `https://www.omdbapi.com/?apikey=84e956e5&s=${userSearch}`
   );
   const moviesWrapper = document.querySelector(".movies");
   const movieData = await movieResponse.json();
-  moviesWrapper.innerHTML = movieData.Search.map((movie) => moviesHTML(movie)).join("");
+  const showMovie = movieData.Search.map((movie) => moviesHTML(movie)).join("");
+  moviesWrapper.innerHTML += showMovie
+
 }
 
 function moviesHTML(movie) {
-  return `<div class="movie">
+  return `<div class="movie" onclick="getTitle('${movie.imdbID}')">
               <figure class="movie__img--wrapper">
-                <img src="${movie.Poster}" alt="" onclick="getTitle()'>
+                <img src="${movie.Poster}">
               </figure>
-              <h2 class="movie__title">${movie.Title}</h2>
+              <h2  class="movie__title ">${movie.Year}</h2>
               <p class="year">${movie.Year}</p>
               <p class="type">${movie.Type}</p>
+              <p>${movie.imdbID}</p>
             </div>`;
 }
 
-function getTitle(){
-  console.log('this worked')
+function getTitle(movieTitle){
+  localStorage.setItem('movieTitle', movieTitle)
+  window.location.href = `${window.location.origin}/movieDetails.html`
 }
 
-fetchMovie();
+
+fetchMovie(userSearch);
