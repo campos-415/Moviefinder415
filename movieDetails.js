c5a9338bb224cdc96c5ec50561c008f9f5f214ba
Cesar Campos <camposss.415@gmail.com>
2023-01-22 21:23:06 -0800

    working on the single movie page

--- movieDetails.js
+++ movieDetails.js
@@ -1,25 +1,27 @@
-let movieInfo = localStorage.getItem('movieTitle')
+let movieInfo = localStorage.getItem("movieTitle");
 
+const movieDetailWrapper = document.querySelector(".movie")
+console.log(movieDetailWrapper)
 async function showMovieDetails(movieInfo) {
-  const movieDetailWrapper = document.querySelector('.movie')
-  const movieResponse = await fetch(`https://www.omdbapi.com/?apikey=84e956e5&i=${movieInfo}`)
-  const movieData = await movieResponse.json()
-  const movieDetails = movieData
-  let movieDetailsHTML = showDetails(movieDetails)
-  // movieDetailWrapper.innerHTML += movieDetailsHTML
-  console.log(movieDetailsHTML)
+  const movieResponse = await fetch(
+    `https://www.omdbapi.com/?apikey=84e956e5&i=${movieInfo}`
+    );
+    const movieData = await movieResponse.json();
+    const movieDetails = movieData;
+    const movieDetailsHTML = showDetails(movieDetails);
+    console.log(movieDetailsHTML);
+    movieDetailWrapper.innerHTML += movieDetailsHTML;
 }
 
-
-function showDetails(movie){
+function showDetails(movie) {
   return `<div class="container">
-  <div class="row">
-    <h2 class="movie__title">${movie.Title}</h2>
-    <figure class="movie__img--wrapper">
-      <img src="${movie.Poster}" alt="" class="movie__img">
-    </figure>
-    <p class="movie__plot">${movie.Plot}</p>
-  </div>
-</div>`
+            <div class="row">
+              <h2 class="movie__title">${movie.Title}</h2>
+              <figure class="movie__img--wrapper">
+                <img src="${movie.Poster}" alt="" class="movie__img">
+              </figure>
+              <p class="movie__plot">${movie.Plot}</p>
+            </div>
+          </div>`;
 }
-showMovieDetails(movieInfo)
+showMovieDetails(movieInfo);
--- movies.js
+++ movies.js
@@ -6,26 +6,24 @@ async function fetchMovie(userSearch) {
   const moviesWrapper = document.querySelector(".movies");
   const movieData = await movieResponse.json();
   const showMovie = movieData.Search.map((movie) => moviesHTML(movie)).join("");
-  moviesWrapper.innerHTML += showMovie
-
+  moviesWrapper.innerHTML += showMovie;
 }
 
 function moviesHTML(movie) {
   return `<div class="movie" onclick="getTitle('${movie.imdbID}')">
-              <figure class="movie__img--wrapper">
-                <img src="${movie.Poster}">
-              </figure>
-              <h2  class="movie__title ">${movie.Year}</h2>
-              <p class="year">${movie.Year}</p>
-              <p class="type">${movie.Type}</p>
-              <p>${movie.imdbID}</p>
-            </div>`;
+            <figure class="movie__img--wrapper">
+              <img src="${movie.Poster}">
+            </figure>
+            <h2  class="movie__title ">${movie.Year}</h2>
+            <p class="year">${movie.Year}</p>
+            <p class="type">${movie.Type}</p>
+            <p>${movie.imdbID}</p>
+          </div>`;
 }
 
-function getTitle(movieTitle){
-  localStorage.setItem('movieTitle', movieTitle)
-  window.location.href = `${window.location.origin}/movieDetails.html`
+function getTitle(movieTitle) {
+  localStorage.setItem("movieTitle", movieTitle);
+  window.location.href = `${window.location.origin}/movieDetails.html`;
 }
-
 
 fetchMovie(userSearch);
