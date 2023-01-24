e908f3642cbf5442fa78c38c6b30e9d29e65182f
Cesar Campos <camposss.415@gmail.com>
2023-01-24 00:58:06 -0800

    .

--- movies.js
+++ movies.js
@@ -4,20 +4,32 @@ async function fetchMovie(userSearch, filter) {
     `https://www.omdbapi.com/?apikey=84e956e5&s=${userSearch}`
     );
   const movieData = await movieResponse.json();
-  const filterMovieValue = filterMovies(filter)
-  if (filterMovieValue === 'A__TO__Z') {
-    return movieData.Search.sort((a, b) => a.Year - b.Year)
-  }
   const moviesWrapper = document.querySelector(".movies");
+  console.log(movieData)
+  
+  if (filter === "OLD__TO__NEW"){
+    movieData.Search.sort((a, b) => a.Year - b.Year)
+    console.log(movieData.Search)
+  }
+  else if (filter === "NEW__TO__OLD"){
+    movieData.Search.sort((a, b) => b.Year - a.Year)
+    console.log(movieData.Search)
+  }
+
   const showMovie = movieData.Search.map((movie) => moviesHTML(movie)).join("");
   moviesWrapper.innerHTML += showMovie;
+  console.log(filter)
 }
+ 
 
-// fetchMovie(userSearch)
+
+// setTimeout(() => {
+  fetchMovie(userSearch)
+// },1000)
+
 function filterMovies(event){
-  console.log(event.target.value)
-  console.log(fetchMovie(userSearch,event.target.value))
-  return fetchMovie(userSearch, event.target.value)
+  fetchMovie(userSearch, event.target.value)
+  // console.log(event.target.value)
 }
 
 function getTitle(movieTitle) {
@@ -33,6 +45,5 @@ function moviesHTML(movie) {
             <h2  class="movie__title ">${movie.Title}</h2>
             <p class="year">${movie.Year}</p>
             <p class="type">${movie.Type}</p>
-            <p>${movie.imdbID}</p>
           </div>`;
 }
