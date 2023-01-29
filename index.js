6d674f137cb31d6e4c2d1b9292a2a47d8bed379d
Cesar Campos <camposss.415@gmail.com>
2023-01-29 12:30:25 -0800

    added a menu for mobile devices

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 
+function openMenu() {
+  document.body.classList += ' menu__open'
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu__open')
+}
+
+
 function searchMovie(event) {
   event.preventDefault()
   window.location.href = `${window.location.origin}/movies.html`
